c2b89dc4b33075c8f5df586133f9306124b74326
Rhonal Chirinos <rhonalchirinos@gmail.com>
2025-07-14 12:00:27 -0300

    Simplify HealthInteractor provider registration
    Replaces the explicit provider object for HealthInteractor with a shorthand array entry in HomeModule for cleaner code.

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,11 +4,6 @@ import { HealthInteractor } from '@core/home/application/health.interactor';
 
 @Module({
   controllers: [AppController],
-  providers: [
-    {
-      provide: HealthInteractor,
-      useClass: HealthInteractor,
-    },
-  ],
+  providers: [HealthInteractor],
 })
 export class HomeModule {}
